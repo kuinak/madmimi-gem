908da16c42c8c04b4edb4c3202e1418ac63e93fd
Evan Petrie <ejp@kuinak.com>
2013-07-09 09:52:51 -0700

    add options for add_to_list
    should be able to pass in optional values for first_name, last_name

--- lib/madmimi.rb
+++ lib/madmimi.rb
@@ -100,8 +100,8 @@ class MadMimi
     do_request(AUDIENCE_MEMBERS_PATH, :post, :csv_file => csv_data)
   end
 
-  def add_to_list(email, list_name)
-    do_request("#{NEW_LISTS_PATH}/#{URI.escape(list_name)}/add", :post, :email => email)
+  def add_to_list(email, list_name, options = {})
+    do_request("#{NEW_LISTS_PATH}/#{URI.escape(list_name)}/add", :post, {:email => email}.merge(options))
   end
 
   def remove_from_list(email, list_name)
